1653ab5df1816f957a863d4319f01552cd000c28
dahye <dhk8233@gmail.com>
2022-12-08 08:30:29 +0900

    movie_search_app_react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/header/Header'
 import Home from './pages/home/Home'
 import Movie from './pages/movieDetail/Movie'
@@ -6,19 +6,23 @@ import MovieList from './components/movieList/MovieList'
 import NotFound from './components/notFound/NotFound'
 
 const App = () => {
+
+
   return (
-    <div>
-      <Router>
+
+      <BrowserRouter>
        <Header />
         <Routes>
-          <Route path='/' index element={<Home />}></Route>
-          <Route path='movie/:id' element={<Movie />}></Route>
-          <Route path='movies/:type' element={<MovieList />}></Route>
-          <Route path='/*' element={<NotFound />}></Route>
+          <Route path="/Movie_React_App" index element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="movies/:type" element={<MovieList />} />
+          <Route path="/*" element={<NotFound />} />
 
         </Routes>
-      </Router>
-    </div>
+      </BrowserRouter>
+
+            
+  
   )
 }
 
